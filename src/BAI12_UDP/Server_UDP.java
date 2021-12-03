d54369b3eeb9f76ac227130aab57c4805fb310c2
Tran Quang Linh <linhdautu@gmail.com>
2021-12-03 23:45:24 +0700

    episode 13

---
+++ src/BAI12_UDP/Server_UDP.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI12_UDP;
+
+import BAI11_UDP.*;
+import BAI4_UDP.*;
+import BAI1_UDP.*;
+import Function.Main_function;
+import static Function.Main_function.phanTichSoNguyen;
+import static bai1_tcp.Server_TCP.CHAR_55;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.List;
+
+/**
+ *
+ * @author azure Tran
+ */
+public class Server_UDP extends Main_function {
+     public final static int SERVER_PORT = 7; // Cổng mặc định của Echo Server
+    public final static byte[] BUFFER = new byte[4096]; // Vùng đệm chứa dữ liệu cho gói tin nhận
+   
+    public static void main(String[] args) {
+        DatagramSocket ds = null;
+        try {
+            System.out.println("Binding to port " + SERVER_PORT + ", please wait  ...");
+            ds = new DatagramSocket(SERVER_PORT); // Tạo Socket với cổng là 7
+            System.out.println("Server started ");
+            System.out.println("Waiting for messages from Client ... ");
+ 
+            while (true) { // Tạo gói tin nhận
+                DatagramPacket number = new DatagramPacket(BUFFER, BUFFER.length);
+                ds.receive(number); // Chờ nhận gói tin gởi đến
+                   String number1 = new String(number.getData(), 0, number.getLength());
+                System.out.println("Received: " + number1);
+                //list number isprimenumber<number
+                String result="";
+                
+        // phân tích số nguyên dương n
+          List<Integer> listNumbers = phanTichSoNguyen(Integer.parseInt(number1));
+        // in kết quả ra màn hình
+          int size = listNumbers.size();
+        
+          for (int i = 0; i < size - 1; i++) {
+           
+            result+=Integer.toString(listNumbers.get(i))+"x";
+        }
+           
+             result+=listNumbers.get(size - 1);
+                result+="\n sum is :"+tinhTong(Integer.parseInt(number1));
+                           // Tạo gói tin gởi chứa dữ liệu vừa nhận được
+                DatagramPacket outsending = new DatagramPacket(result.getBytes(), result.length(),
+                        number.getAddress(), number.getPort());
+                
+                ds.send(outsending);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (ds != null) {
+                ds.close();
+            }
+        }
+    }
+}
--- src/Function/Main_function.java
+++ src/Function/Main_function.java
@@ -111,5 +111,23 @@ public class Main_function {
         }
         return (count);
    }
+      //eposide 11
+      /**
+     * Tính số fibonacci thứ n
+     * 
+     * @param n: chỉ số của số fibonacci tính từ 0 
+     *           vd: F0 = 0, F1 = 1, F2 = 1, F3 = 2
+     * @return số fibonacci thứ n
+     */
+    public static int fibonacci(int n) {
+        if (n < 0) {
+            return -1;
+        } else if (n == 0 || n == 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+     
       
 }
